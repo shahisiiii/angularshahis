a04d2b3d7ede9bd79b0f72a3e90b176f98b2bff8
jabeel shahis ak <jabeelshahisak@gmail.com>
2023-12-13 10:31:17 +0530

    errors occured

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Day1Component } from './day1/day1.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'d1',component:Day1Component}
+];
---
+++ src/app/day1/day1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-day1',
+  standalone: true,
+  imports: [],
+  templateUrl: './day1.component.html',
+  styleUrl: './day1.component.css'
+})
+export class Day1Component {
+
+}
